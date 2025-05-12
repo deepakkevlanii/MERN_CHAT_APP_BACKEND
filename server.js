2b83ebac95c0a904aca24464bd2517a7135e9e25
Deepak Kevlani <158258171+deepakkevlanii@users.noreply.github.com>
2025-05-12 18:12:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const server = app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`.yellow.bold)
 );
